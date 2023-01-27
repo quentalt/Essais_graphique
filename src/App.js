188ab0b5e912fe15f62c4a21e5852801701a6aff
quentalt <quentin.altieri@gmail.com>
2023-01-27 17:56:05 +0100

    update

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import Sketch from "react-p5";
 
 function App() {
     const [circles, setCircles] = useState([]);  // initial state with one circle
-    const [curvePoints, setCurvePoints] = useState([]);  // initial state with empty array for curve points
+    const [lines, setLines] = useState([]); // initial state with no lines
+
     const setup = (p5, canvasParentRef) => {
         p5.createCanvas(800, 500).parent(canvasParentRef);
     }
 
+    const mousePressed = p5 => {
+         setCircles([...circles, { x: p5.mouseX, y: p5.mouseY }]);
+    if (circles.length >= 2) {
+        setLines([...lines, { x1: circles[circles.length - 2].x, y1: circles[circles.length - 2].y, x2: circles[circles.length - 1].x, y2: circles[circles.length - 1].y
+         }]);
+    }
+    }
+    
     const draw = p5 => {
         p5.background(255, 204, 0);
         p5.fill(255);
@@ -15,42 +24,36 @@ function App() {
         if (circles.length > 0) {
             circles.forEach(circle => {
                 p5.circle(circle.x, circle.y, 50, 50);
-                p5.line(circle.x, circle.y, p5.mouseX, p5.mouseY);
+            });
+        }
+        if (lines.length > 0) {
+            lines.forEach(line => {
+                p5.line(line.x1, line.y1, line.x2, line.y2);
             });
         }
     }
 
-
-
-
-    /*const mousePressed = p5 => {
-        setCircles([...circles, { x: p5.mouseX, y: p5.mouseY }]);
-    }*/
-
-    const mouseDragged = p5 => {
+    /* const mouseDragged = p5 => {
         const index = circles.findIndex(circle => p5.dist(circle.x, circle.y, p5.mouseX, p5.mouseY) < 50);
         if (index !== -1) {
             const newCircles = [...circles];
             newCircles[index] = { x: p5.mouseX, y: p5.mouseY };
             setCircles(newCircles);
-            setCurvePoints([...curvePoints, { x: p5.mouseX, y: p5.mouseY }]);
         }
-    }
+    } */
 
     const handleButtonClick = () => {
         setCircles([...circles, { x: 50, y: 50 }]);  // add new circle to state
     }
 
-    return (
-        <>
-            <Sketch
-                setup={setup}
-                draw={draw}
-                mouseDragged={mouseDragged}
-            />
-            <button onClick={handleButtonClick}>Add Circle</button>  // add button
-        </>
-    );
-}
 
+
+    return (
+        <div className="App">
+            <Sketch setup={setup} draw={draw} mousePressed={mousePressed} />
+            <button onClick={handleButtonClick}>Add Circle</button>
+        </div>
+    );
+    
+}
 export default App;
